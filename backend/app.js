79fe191cfbf71e943b5838c58c6f04cefa9dff40
msnigatsu <msnigatsu@gmail.com>
2026-02-04 00:58:39 +0800

    更新：初階任務

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,7 @@ app.get('/healthcheck', (req, res) => {
   message: 'OK'
 })
 })
+
 app.use('/api/credit-package', creditPackageRouter)
 app.use('/api/coaches/skill', skillRouter)
 app.use('/api/users', usersRouter)
